bfbf0f58718ce36e985173885f28c774e7034a3c
Antonin <antoninpicard0@gmail.com>
2022-11-29 11:21:02 +0100

    setup

---
+++ c#/Convert a Number to a String!.cs
@@ -0,0 +1,20 @@
+// We need a function that can transform a number (integer) into a string.
+
+// What ways of achieving this do you know?
+
+// Examples (input --> output):
+// 123  --> "123"
+// 999  --> "999"
+// -100 --> "-100"
+
+
+
+using System;
+
+public class Kata
+{
+  public static string NumberToString(int num)
+  {
+    return num.ToString();
+  }
+}
