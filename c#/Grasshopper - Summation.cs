18bad272b56606f7b87eb66239e4a62dd4d528a5
Antonin <antoninpicard0@gmail.com>
2022-11-29 11:52:25 +0100

    change

---
+++ c#/Grasshopper - Summation.cs
@@ -0,0 +1,28 @@
+// Summation
+// Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+
+// For example:
+
+// summation(2) -> 3
+// 1 + 2
+
+// summation(8) -> 36
+// 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8
+
+using System;
+
+public static class Kata 
+{
+    
+    public static int summation(int num)
+    {
+      int count;
+      count = 0;
+      for (int i = 0; i <= num; i++)
+      {
+          count += i;
+      }
+      return count;
+      
+    }
+}
---
+++ js/Training JS#4.js
@@ -0,0 +1,48 @@
+// In javascript, Array is one of basic data types. To define an empty array, you can use var arr=new Array() or var arr=[]
+
+// Array has an attribute: length, if there is an array named arr, using arr.length to know how many elements are contained in the array.
+
+// Each element in the array has an index, use arr[index] to get the value of element.
+
+// index always start from 0, so the first element of array is arr[0], the last element of array is arr[arr.length-1].
+
+// If we want to add new elements to the array, you can use the array method: push(). It can add an element to the end of the array. Instead, if we want to remove the last element of the array, you can use the array method: pop(). for example:
+
+// var arr=[1,2,3];     //define an array arr contains elements 1 2 3
+// arr.push(4);         //add element 4 to arr
+// console.log(arr)     //[1,2,3,4]
+// arr.pop();           //remove the last element from arr
+// console.log(arr)     //[1,2,3]
+// Task
+// I've written five functions. Each function receives a parameter arr which is an array. Complete the functions using arr inside the function bodies.
+
+//     1. getLength(arr)    should return length of arr
+//     2. getFirst(arr)     should return the first element of arr
+//     3. getLast(arr)      should return the last element of arr
+//     4. pushElement(arr)  should push an element to arr, and then return arr
+//     5. popElement(arr)   should pop an element from arr, and then return arr
+
+function getLength(arr){
+    //return length of arr
+    return arr.length;
+  }
+  function getFirst(arr){
+    //return the first element of arr
+    return arr[0];
+  }
+  function getLast(arr){
+    //return the last element of arr
+    return arr[arr.length-1];
+  }
+  function pushElement(arr){
+    var el=1;
+    //push el to arr
+    
+    arr.push(4)
+    return arr
+  }
+  function popElement(arr){
+    //pop an element from arr
+    arr.pop(3)
+    return arr
+  }
