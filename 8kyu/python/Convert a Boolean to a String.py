2e35eead7b1c72d611fe5c5f63ab93d0a1b48304
Antonin <antoninpicard0@gmail.com>
2022-12-05 14:26:18 +0100

    Bool to String !

---
+++ 8kyu/python/Convert a Boolean to a String.py
@@ -0,0 +1,9 @@
+# Implement a function which convert the given boolean value into its string representation.
+
+# Note: Only valid inputs will be given.
+
+def boolean_to_string(b):
+    if b == True:
+        return "True"
+    elif b == False:
+        return "False"
