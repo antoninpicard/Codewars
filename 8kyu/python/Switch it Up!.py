a277b36d7bad5a5f2a207fdc1fb42f9d583e4ce5
Antonin <antoninpicard0@gmail.com>
2022-12-05 14:37:05 +0100

    Switch it Up!.py

---
+++ 8kyu/python/Switch it Up!.py
@@ -0,0 +1,29 @@
+# When provided with a number between 0-9, return it in words.
+
+# Input :: 1
+
+# Output :: "One".
+
+# If your language supports it, try using a switch statement.
+
+def switch_it_up(number):
+    if number == 1:
+        return "One"
+    elif number == 0:
+        return "Zero"
+    elif number == 2:
+        return "Two"
+    elif number == 3:
+        return "Three"
+    elif number == 4:
+        return "Four"
+    elif number == 5:
+        return "Five"
+    elif number == 6:
+        return "Six"
+    elif number == 7:
+        return "Seven"
+    elif number == 8:
+        return "Eight"
+    elif number == 9:
+        return "Nine"
