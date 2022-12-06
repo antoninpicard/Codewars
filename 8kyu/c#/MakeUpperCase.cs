4d3f4a4c1ea58b9d236ab1a99c69cb8d3f269880
Antonin <antoninpicard0@gmail.com>
2022-12-06 09:46:18 +0100

    MakeUpperCase.cs

---
+++ 8kyu/c#/MakeUpperCase.cs
@@ -0,0 +1,10 @@
+// Write a function which converts the input string to uppercase.
+
+ public class Kata
+  {
+        public static string MakeUpperCase(string str)
+        {
+             return str.ToUpper();
+        }
+
+  }
