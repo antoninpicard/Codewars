fbe04db0fe2662ce467808f87e25fa72c2cd8bbc
Antonin <antoninpicard0@gmail.com>
2023-01-09 11:16:57 +0100

    Vowel Count.cs

---
+++ 7kuy/c#/Vowel Count.cs
@@ -0,0 +1,7 @@
+// Return the number (count) of vowels in the given string.
+
+// We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+// The input string will only consist of lower case letters and/or spaces.
+
+// https://www.codewars.com/kata/54ff3102c1bad923760001f3
