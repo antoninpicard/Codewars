a827cecc2b73da0ac6213df7db2855141a9a1c0e
Antonin <antoninpicard0@gmail.com>
2023-01-15 20:13:35 +0100

    Remove First and Last Character.cs

---
+++ 8kyu/c#/Remove First and Last Character.cs
@@ -0,0 +1,2 @@
+// It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
+// https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0
