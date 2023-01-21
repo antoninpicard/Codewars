421eaa1430de686cbc51dc0aee0fbf6a6cbc1a90
Antonin <antoninpicard0@gmail.com>
2023-01-21 12:16:54 +0100

    DoubleChar.cs

---
+++ 8kyu/c#/Double Char.cs
@@ -0,0 +1,9 @@
+// Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+// Examples (Input -> Output):
+// * "String"      -> "SSttrriinngg"
+// * "Hello World" -> "HHeelllloo  WWoorrlldd"
+// * "1234!_ "     -> "11223344!!__  "
+// Good Luck!
+
+// https://www.codewars.com/kata/56b1f01c247c01db92000076
