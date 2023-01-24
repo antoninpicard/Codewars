e393a43071de44a904fd7f8832961453ae378df1
Antonin <antoninpicard0@gmail.com>
2023-01-24 08:54:28 +0100

    python

---
+++ 8kyu/python/5 without numbers.py
@@ -0,0 +1,7 @@
+# Write a function that always returns 5
+
+# Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+# Good luck :)
+
+# https://www.codewars.com/kata/59441520102eaa25260000bf
