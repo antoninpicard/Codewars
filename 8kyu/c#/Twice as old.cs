414090fa449e0225922e1af81f2b092b4cd99092
Antonin <antoninpicard0@gmail.com>
2023-01-26 08:59:53 +0100

    cs and php

---
+++ 8kyu/c#/Twice as old.cs
@@ -0,0 +1,7 @@
+// Your function takes two arguments:
+
+// current father's age (years)
+// current age of his son (years)
+// Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old). The answer is always greater or equal to 0, no matter if it was in the past or it is in the future.
+
+// https://www.codewars.com/kata/5b853229cfde412a470000d0
---
+++ 8kyu/php/Third Angle of a Triangle.php
@@ -0,0 +1,10 @@
+<!-- 
+    
+You are given two interior angles (in degrees) of a triangle.
+
+Write a function to return the 3rd.
+
+Note: only positive integers will be tested. 
+https://www.codewars.com/kata/5a023c426975981341000014
+
+-->
