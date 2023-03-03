1c205a584bbb14d975b9ada0e1e1f78cbdb2f331
Antonin <antoninpicard0@gmail.com>
2023-03-03 11:07:05 +0100

    The 'if' function

---
+++ 8kyu/c++/The 'if' function.cpp
@@ -0,0 +1,11 @@
+// Create a function called _if which takes 3 arguments: a value bool and 2 functions (which do not take any parameters): func1 and func2
+
+// When bool is truthy, func1 should be called, otherwise call the func2.
+
+// Example:
+// void TheTrue() { std::cout << "true" }
+// void TheFalse() { std::cout << "false" }
+// _if(true, TheTrue, TheFalse);
+// Logs 'true' to the console.
+
+// https://www.codewars.com/kata/54147087d5c2ebe4f1000805
