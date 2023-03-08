3dc926e7e12c2b912ef48eba093edad46c7ec2fc
Antonin <antoninpicard0@gmail.com>
2023-03-08 16:26:33 +0100

    Lario and Muigi Pipe Problem.c++

---
+++ 8kyu/c++/Lario and Muigi Pipe Problem.cpp
@@ -0,0 +1,11 @@
+// The pipes connecting your level's stages together need to be fixed before you receive any more complaints.
+
+// Pipes list is correct when each pipe after the first index is greater (+1) than the previous one, and that there is no duplicates.
+
+// Task
+// Given the a list of numbers, return a fixed list so that the values increment by 1 for each index from the minimum value up to the maximum value (both included).
+
+// Example
+// Input:  1,3,5,6,7,8 Output: 1,2,3,4,5,6,7,8
+
+// https://www.codewars.com/kata/56b29582461215098d00000f
